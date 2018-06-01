91343dade4338ae1663f09b07b0e8959686d4ce5
Daniel Venturini <daniellagarto0@gmail.com>
2018-06-01 17:46:11 -0300

    Arrumando o proto

--- Server/Server.py
+++ Server/Server.py
@@ -1,51 +1,26 @@
-from concurrent import futures
-from Agenda_pb2 import Contato
-import Agenda_pb2_grpc
+import MemoryGame_pb2_grpc
 import Network
+import Random
 import grpc
 import time
 import sys
 
-class AgendaServicer(Agenda_pb2_grpc.AgendaServicer):
+# para facilitar, o id da partida sera a semente para o embaralhamento
+class MemoryGameServicer(MemoryGame_pb2_grpc.MemoryGameServicer):
 
     def __init__(self):
-        self.Agenda = []
+        # hash com o id da partida
+        self.partidas = {}
 
-    def Adicionar(self, contato, context):
-        print("Adicionando contato: " + contato.nome + " " + contato.telefone)
+    def Jogar(self, Id):
+        pass
 
-        self.Agenda.append(contato)
-        return contato
+    def Assistir(self, Id):
+        pass
 
+    def FimJogo(self, Id):
+        pass
 
-    def Remover(self, contato, context):
-        print("Removendo o contato {} {}".format(contato.nome, contato.telefone))
-
-        for contatoLista in self.Agenda:
-            if(contatoLista.nome == contato.nome or contatoLista.telefone == contato.telefone):
-                print("Encontrado contato para remover: " + contato.nome + " " + contato.telefone)
-                self.Agenda.remove(contatoLista)
-                return contatoLista
-
-        return self.contatoVazio(contato)
-
-
-    def Consultar(self, contato, context):
-        print("Procurando o contato {} {}".format(contato.nome, contato.telefone))
-
-        for contatoLista in self.Agenda:
-            if(contatoLista.nome == contato.nome or contatoLista.telefone == contato.telefone):
-                print("Encontrado contato: " + contato.nome + " " + contato.telefone)
-                return contatoLista
-
-        return self.contatoVazio(contato)
-
-    def contatoVazio(self, contato):
-        print("Nenhum contato " + contato.nome + " " + contato.telefone + ". Retornano contato vazio.")
-        contato = Contato()
-        contato.nome = "Nao encontrado"
-        contato.telefone = "Nao encontrado"
-        return contato
 
 def server():
     ip, broadcast = Network.getIP_BC()
@@ -55,7 +30,7 @@ def server():
         return
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    Agenda_pb2_grpc.add_AgendaServicer_to_server(AgendaServicer(), server)
+    MemoryGame_pb2_grpc.add_MemoryGameServicer_to_server(MemoryGameServicer(), server)
     #server.add_insecure_port('[::]:50051')
     server.add_insecure_port(ip + ':50051')
     server.start()
--- src/emRede/Conexao.java
+++ src/emRede/Conexao.java
@@ -5,23 +5,23 @@
  */
 package emRede;
 
-import io.grpc.ManagedChannel;
-import java.util.Random;
-import com.google.protobuf.Message;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.Status;
+import java.util.concurrent.CountDownLatch;
 import io.grpc.StatusRuntimeException;
+import io.grpc.ManagedChannelBuilder;
+import java.util.concurrent.TimeUnit;
 import io.grpc.stub.StreamObserver;
+import com.google.protobuf.Message;
+import java.util.logging.Logger;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannel;
 import java.io.IOException;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Random;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.Logger;
+import java.util.Random;
+import io.grpc.Status;
+import java.util.List;
 
 /**
  *
